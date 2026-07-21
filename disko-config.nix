12bd18cc1f58607471072aa4629e2b0c5f9abd71
nikelborm <evadev@duck.com>
2026-07-22 00:23:37 +0300

    quick save: Wed Jul 22 12:23:37 AM MSK 2026

--- disko-config.nix
+++ disko-config.nix
@@ -1,40 +1,82 @@
 {
-  disko.devices.disk.main = {
-    device = "/dev/sda";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          priority = 1;
-          name = "ESP";
-          start = "1M";
-          end = "512M";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "umask=0077" ];
+  disko.devices = {
+    disk.main = {
+      # device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 1;
+            name = "ESP";
+            start = "1M";
+            end = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypted";
+              # extraOpenArgs = [ ];
+              settings = {
+                # if you want to use the key for interactive login be sure there is no trailing newline
+                # for example use `echo -n "password" > /tmp/secret.key`
+                # keyFile = "/tmp/secret.key";
+                allowDiscards = true;
+              };
+              # additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+              passwordFile = "/tmp/secret.key";
+              content = {
+                type = "lvm_pv";
+                vg = "pool";
+              };
+            };
           };
         };
-        plainSwap = {
+      };
+    };
+
+    lvm_vg.pool = {
+      type = "lvm_vg";
+      lvs = {
+        root = {
           size = "100%";
-          priority = 2;
+          content = {
+            type = "filesystem";
+            format = "btrfs";
+            extraArgs = [ "-f" ]; # Override existing partition
+            # not sure if it's possible to apply it when /@ should be mounted before /partition-root
+            # mountpoint = "/partition-root";
+            # mountOptions = [
+            #   "defaults"
+            # ];
+            subvolumes = {
+              "/@" = {
+                mountpoint = "/";
+              };
+              "/@home" = {
+                # mountOptions = [ "compress=zstd" ];
+                mountpoint = "/home";
+              };
+            };
+          };
+        };
+        swap = {
+          size = "100%";
           content = {
             type = "swap";
             priority = 0;
             discardPolicy = "both";
             randomEncryption = false;
             resumeDevice = false;
-          };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
           };
         };
       };
