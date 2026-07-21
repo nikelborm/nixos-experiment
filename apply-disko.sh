701a7ec9eb3407df7611872893935587bbb7b16c
nikelborm <evadev@duck.com>
2026-07-21 13:05:13 +0300

    quick save: Tue Jul 21 01:05:13 PM MSK 2026

---
+++ apply-disko.sh
@@ -0,0 +1,3 @@
+nix --experimental-features "nix-command flakes" \
+  run github:nix-community/disko/latest -- \
+  --mode destroy,format,mount ./disko-config.nix
--- disko-config.nix
+++ disko-config.nix
@@ -1,11 +1,43 @@
 {
   disko.devices.disk.main = {
-    type = "disk";
     device = "/dev/sda";
-
+    type = "disk";
     content = {
       type = "gpt";
-
+      partitions = {
+        ESP = {
+          priority = 1;
+          name = "ESP";
+          start = "1M";
+          end = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        plainSwap = {
+          size = "100%";
+          priority = 2;
+          content = {
+            type = "swap";
+            priority = 0;
+            discardPolicy = "both";
+            randomEncryption = false;
+            resumeDevice = false;
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+      };
     };
   };
 }
