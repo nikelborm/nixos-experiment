4044ef8e7b2db4e3aa123cde6ee64ca1ca6d29f7
nikelborm <evadev@duck.com>
2026-07-22 12:25:18 +0300

    quick save: Wed Jul 22 12:25:18 PM MSK 2026

--- apply-disko.sh
+++ apply-disko.sh
@@ -1,3 +1,6 @@
+read -rs -p "LUKS passphrase: " pw; echo
+printf %s "$pw" > /tmp/secret.key; unset pw
+
 nix --experimental-features "nix-command flakes" \
   run github:nix-community/disko -- \
   --mode destroy,format,mount --show-trace ./disko-config.nix
