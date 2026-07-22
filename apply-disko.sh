66a1b5e7541ce0a2686473d713a4d99db52078ae
nikelborm <evadev@duck.com>
2026-07-22 12:06:12 +0300

    quick save: Wed Jul 22 12:06:12 PM MSK 2026

--- apply-disko.sh
+++ apply-disko.sh
@@ -1,3 +1,3 @@
 nix --experimental-features "nix-command flakes" \
-  run github:nix-community/disko/latest -- \
-  --mode destroy,format,mount ./disko-config.nix
+  run github:nix-community/disko -- \
+  --mode destroy,format,mount --show-trace ./disko-config.nix
